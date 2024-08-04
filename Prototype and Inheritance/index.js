7e71720157c84624e92d21bff0fe05b9cd555c49
Nikita <nikita.kuzko2013@gmail.com>
2024-08-04 21:52:31 +0300

    make prototype and inheritance task

--- Prototype and Inheritance/index.js
+++ Prototype and Inheritance/index.js
@@ -1,49 +1,32 @@
-const allRooms = document.getElementById('rooms');
-const createRoomForm = document.getElementById('create-room');
-const hotel = JSON.parse(localStorage.getItem('hotel')) || [];
-const bookRoomForm = document.getElementById('book-room');
+function Book (title, author, year){
+  this.title = title;
+  this.author = author;
+  this.year = year;
+}
 
-createRoomForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const roomNumber = document.getElementById('room-number-input').value;
-  const newRoom = new CreateRoom(roomNumber);
-  hotel.push(newRoom);
-  localStorage.setItem('hotel', JSON.stringify(hotel));
-  document.getElementById('room-number-input').value = '';
-  console.log(newRoom);
-})
+function EBook (obj, fileSize){
+  Object.assign(this, obj)
+  this.fileSize = fileSize;
+  this.download = () => {
+    console.log("download");
+  }
+}
 
-bookRoomForm.addEventListener('submit', (e)=>{
-  e.preventDefault();
-  const visitor = document.getElementById('visitor').value;
-  const roomN = document.getElementById('book-room-input').value;
-  const roomToBook = hotel.filter(obj => obj.number == roomN);
-  const allOtherRooms = hotel.filter(obj => obj.number != roomN);
+Book.prototype.info = function() {
+  console.log(this);
+  console.log(`Book ${this.title}, author ${this.author}`);
+}
 
-  BookRoom.call(roomToBook[0], visitor);
-  allOtherRooms.push(roomToBook[0]);
-  localStorage.setItem('hotel', JSON.stringify(allOtherRooms));
+EBook.prototype = Book.prototype
 
-  document.getElementById('book-room-input').value = '';
-  document.getElementById('visitor').value = '';
-  console.log('room is booked');
-})
+const firsBook = new Book('Shining', 'S.King', 1969);
+const firstEBook = new EBook(firsBook, 1024);
 
-document.getElementById('show-info').addEventListener('click', ()=>{
-  let info = JSON.parse(localStorage.getItem('hotel'));
-  let bookedRooms = info.filter(room => room.isBooked);
-  console.log(bookedRooms);
-})
 
-function CreateRoom (number){
-  this.number = number;
-  this.name = `room${number}`;
-  this.isBooked = false;
-  this.visitor = '';
+console.log(firsBook);
+console.log(firstEBook);
 
-};
+firsBook.info();
+firstEBook.info();
 
-function BookRoom (visitor) {
-  this.isBooked = true;
-  this.visitor = visitor;
-};
+firstEBook.download();
